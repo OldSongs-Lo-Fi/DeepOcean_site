0b945022a717066ed6ed3ffbc76b669decc72100
OldSongs-Lo-Fi <vakulacomp3@gmail.com>
2022-11-20 23:45:58 +0200

    Decoration and first fish

---
+++ calc.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', e => {
+    document.body.style.cssText = `--scrollTop: ${this.scrollY}px`;
+})
+
+//spawner launcher
+/*
+setInterval(spawner, 500);
+
+
+function spawner(){
+    let randomPos = new Map();
+    time = Date.now();
+    alert(time);
+
+    let bubble = window.document.createElement("div", {class:"bubble", style:`background-image: url(images/amogus.png);`})
+}*/
